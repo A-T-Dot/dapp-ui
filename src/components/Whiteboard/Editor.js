81ffaac44f9cdbd0e84637ff19463b8692b9d4a8
Anakorn Kyavatanakij <anakornk@gmail.com>
2019-09-13 12:11:17 +0800

    add links; todo: prevent links duplication

--- src/components/Whiteboard/Editor.js
+++ src/components/Whiteboard/Editor.js
@@ -2,20 +2,21 @@ import React from "react";
 import {
   Image,
   Card,
-  Button
+  Button,
+  Icon
 } from "semantic-ui-react";
 import Draggable from "react-draggable";
 import LinkTo from "react-lineto";
 
 function DraggableNode(props) {
-  let { dragHandlers, node, padding } = props;
+  let { dragHandlers, node, padding, onLinkClicked, onCardClicked } = props;
 
   return (
     <Draggable
       bounds="parent"
       {...dragHandlers}
       defaultPosition={{ x: node.x, y: node.y }}
-      handle='.handle'
+      handle=".handle"
     >
       <Card
         className={`n${node.id}`}
@@ -25,23 +26,26 @@ function DraggableNode(props) {
           left: padding,
           position: "absolute"
         }}
+        onClick={(e) => {onCardClicked(e, node.id)}}
       >
-        <Card.Content header={node.name} className='handle move'/>
+        <Card.Content header={node.name} className="handle move" />
         <Card.Content>
-          <Card.Meta className='break-word'>{node.id}</Card.Meta>
-          <Card.Description className='break-word'>
-            QmPxGdk9e6p1ZgMRvN6QuQGApZ5b8MPHg5EYkkkj3DSGqQ
+          <Card.Meta className="break-word">{node.id}</Card.Meta>
+          <Card.Description className="break-word">
+            description here
           </Card.Description>
         </Card.Content>
         <Card.Content extra>
-          <div className="ui two buttons">
-            <Button basic color="green">
-              Approve
-            </Button>
-            <Button basic color="red">
-              Decline
-            </Button>
-          </div>
+          <Button
+            primary
+            animated="vertical"
+            onClick={(e, data) => {onLinkClicked(e, node.id);}}
+          >
+            <Button.Content hidden>Link</Button.Content>
+            <Button.Content visible>
+              <Icon name="linkify" />
+            </Button.Content>
+          </Button>
         </Card.Content>
       </Card>
     </Draggable>
@@ -63,7 +67,12 @@ export default class Editor extends React.Component {
         },
         { id: 2, x: 200, y: 200, name: "bob.txt" },
         { id: 3, x: 500, y: 500, name: "hello.txt" }
-      ]
+      ],
+      links: [
+        { source: 1, target: 2},
+        { source: 2, target: 3}
+      ],
+      activeLink: null
     };
   }
 
@@ -81,65 +90,93 @@ export default class Editor extends React.Component {
     this.setState({ activeDrags: --this.state.activeDrags });
   };
 
-  bgClicked = (e) => {
+  onBgClicked = (e) => {
 
     let { clientX, clientY, target } = e
     let { padding, clearChosen, chosenNode } = this.props;
+    if(chosenNode) {
+      if(target.classList.contains("bg-grid")) {
 
-    if(target.classList.contains("bg-grid")) {
+        let rect = target.getBoundingClientRect();
+        let x = Math.round(clientX - rect.left) - padding;
+        let y = Math.round(clientY - rect.top) - padding;
 
-      let rect = target.getBoundingClientRect();
-      let x = Math.round(clientX - rect.left) - padding;
-      let y = Math.round(clientY - rect.top) - padding;
-
-      if(chosenNode) {
         this.setState({nodes: [...this.state.nodes, {id: chosenNode.id, name: chosenNode.name,x, y}]})
-      }
-      console.log(x, y);
-    }
 
-    clearChosen();
+        // console.log(x, y);
+      }
+      clearChosen();
+    }
+    // console.log(target.classList);
 
   }
+
+  onLinkClicked = (e, nodeId) => {
+    let { clearChosen } = this.props;
+
+    e.stopPropagation();
+    clearChosen();
+
+    this.setState({
+      activeLink: nodeId
+    })
+    console.log(nodeId);
+  }
+
+  onCardClicked = (e, nodeId) => {
+    let { clearChosen } = this.props;
+    let { activeLink } = this.state;
+
+    e.stopPropagation();
+    clearChosen();
+
+    if(activeLink) {
+      this.setState({
+        activeLink: null,
+        links: [...this.state.links, { source: activeLink, target: nodeId }]
+      });
+    }
+
+  }
+  
 
   render() {
     const dragHandlers = { onStart: this.onStart, onStop: this.onStop, onDrag: this.onDrag };
     let { padding, chosenNode } = this.props;
-
+    let { nodes, links } = this.state;
     return (
       <div
         className={`bg-grid${chosenNode ? " pointer" : ""}`}
         style={{ height: "3000px", width: "3000px", padding: `${padding}px` }}
-        onClick={this.bgClicked}
+        onClick={this.onBgClicked}
       >
-        {this.state.nodes.map((node, index) => {
+        {nodes.map((node, index) => {
           return (
             <DraggableNode
               key={index}
               dragHandlers={dragHandlers}
               node={node}
               padding={padding}
+              onLinkClicked={this.onLinkClicked}
+              onCardClicked={this.onCardClicked}
             />
           );
         })}
-        <LinkTo
-          from="n1"
-          fromAnchor="bottom center"
-          to="n2"
-          toAnchor="top center"
-          within="bg-grid"
-          borderColor="black"
-          delay={true}
-        />
-        <LinkTo
-          from="n2"
-          fromAnchor="bottom center"
-          to="n3"
-          toAnchor="top center"
-          within="bg-grid"
-          borderColor="black"
-          delay={true}
-        />
+        { links.map((link, index) => {
+          return (
+            <LinkTo
+              key={index}
+              from={`n${link.source}`}
+              fromAnchor="bottom center"
+              to={`n${link.target}`}
+              toAnchor="top center"
+              within="bg-grid"
+              borderColor="black"
+              delay={true}
+            />
+          );
+        })}
+
       </div>
     );
   }
