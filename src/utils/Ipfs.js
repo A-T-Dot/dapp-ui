7256e40c4bd8873d39ab41f24a99f8a1c77ae530
Anakorn Kyavatanakij <anakornk@gmail.com>
2019-09-16 13:46:07 +0800

    return CID object for ipfs methods; upload whiteboard to ipfs

--- src/utils/Ipfs.js
+++ src/utils/Ipfs.js
@@ -1,27 +1,62 @@
 import ipfsClient from "ipfs-http-client";
+import CID from "cids";
+
 let ipfs = ipfsClient("localhost", "5001", { protocol: "http" });
 
+// example usage:
+// // dag
+// let cid = await Ipfs.dagPut(nodeObj);
+// let res = await Ipfs.dagGet(cid);
+// console.log(res);
+
+// // normal
+// let cid2 = await Ipfs.add([JSON.stringify(nodeObj)]);
+// console.log(cid2);
+// let res2 = await Ipfs.get(cid2);
+// console.log(JSON.parse(res2));
+
 export default {
-  add: async (files) => {
+  add: async files => {
     try {
-      let result =  await ipfs.add([...files]);
-      let ipfsId = result[0].hash;
-      return ipfsId;
+      let result = await ipfs.add([...files]);
+      let cid = new CID(result[0].hash);
+      return cid;
     } catch (err) {
       console.error(err);
       return err;
     }
   },
-  get: async (cid) => {
+  get: async cid => {
     try {
-      let files = await ipfs.get(cid);
-      files.forEach((file) => {
-        console.log(file.path)
-        console.log(file.content)
-      })
+      console.log(cid.buffer);
+      let files = await ipfs.get(cid.buffer);
+      return files[0].content;
+      
+      // files.forEach(file => {
+      //   console.log(file.path);
+      //   console.log(file.content);
+      // });
+    } catch (err) {
+      console.error(err);
+      return err;
+    }
+  },
+  dagPut: async obj => {
+    try {
+      let cid = await ipfs.dag.put(obj);
+      return cid;
+    } catch (err) {
+      console.error(err);
+      return err;
+    }
+  },
+  dagGet: async (cid, path = '') => {
+    try {
+      let node = await ipfs.dag.get(cid, path);
+      return node.value;
     } catch (err) {
       console.error(err);
       return err;
     }
   }
-}
+};
