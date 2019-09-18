d8adb925051a18b11f9917dae1e1c8a1c3ecc1a0
Anakorn Kyavatanakij <anakornk@gmail.com>
2019-09-19 00:17:04 +0800

    update readme; add utils for cidv0 and content hash conversion

--- src/utils/Ipfs.js
+++ src/utils/Ipfs.js
@@ -58,5 +58,17 @@ export default {
       console.error(err);
       return err;
     }
+  },
+  getContentHashBufFromCIDv0: (cid) => {
+    return cid.multihash.slice(2);
+  },
+  // Usage: Input is string without 0x1220
+  //  Ipfs.getCIDv0fromContentHash(
+  //     "0x4e8ac7dc3a61da3354ebf3ee7ed24b57df4762c2ef318c125f23cdd759362b63"
+  //   )
+  getCIDv0fromContentHashStr: (b) => {
+    let buf = Buffer.concat([Buffer.from("1220", "hex"), Buffer.from(b.slice(2),"hex")]);
+    let cid = new CID(buf);
+    return cid;
   }
 };
