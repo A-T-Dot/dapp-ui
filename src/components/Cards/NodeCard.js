9ca11db6c0c1534dbaf239ee2a7b21f71d6695a9
Anakorn Kyavatanakij <anakornk@gmail.com>
2019-09-19 15:13:20 +0800

    Add node card

--- src/components/Cards/NodeCard.js
+++ src/components/Cards/NodeCard.js
@@ -1,14 +1,30 @@
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
-import { Container, Button, Card, Grid, List } from "semantic-ui-react";
+import { Container, Button, Card, Grid, List, Header } from "semantic-ui-react";
+import { nodeTypeToText } from "../../constants/nodeType";
+import Ipfs from "../../utils/Ipfs";
 
 export default function NodeCard(props) {
   let { link, node } = props;
+  let { sources, nodeType } = node;
   return (
     <Card as={Link} to={link}>
       <Card.Content>
-        <Card.Header className='break-word'>Node #{node.nodeId}</Card.Header>
-        <Card.Description>test</Card.Description>
+        <Header className="break-word" size="small">{Ipfs.getCIDv0fromContentHashStr(node.nodeId).toString()}</Header>
+      </Card.Content>
+      <Card.Content>
+        <Card.Description>
+          <List>
+            <List.Item>
+              <List.Icon name="users" />
+              <List.Content>{nodeTypeToText[nodeType] || "0"}</List.Content>
+            </List.Item>
+            <List.Item>
+              <List.Icon name="linkify" />
+              <List.Content>{sources.length || "0"} cited sources</List.Content>
+            </List.Item>
+          </List>
+        </Card.Description>
       </Card.Content>
     </Card>
   );
