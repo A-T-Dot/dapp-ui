e10b3e6abc2ee32e607c103c88f7775608ef146a
Anakorn Kyavatanakij <anakornk@gmail.com>
2019-09-19 14:41:00 +0800

    show info on gecard

--- src/components/Cards/GeCard.js
+++ src/components/Cards/GeCard.js
@@ -4,12 +4,32 @@ import { Container, Button, Card, Grid, List } from "semantic-ui-react";
 
 export default function GeCard(props) {
   let { link, ge } = props;
+
+  let { totalStaked, totalInvested, members, tcxIds } = ge;
   return (
     <Card as={Link} to={link}>
       <Card.Content>
-        <Card.Header>GE #{ge.geId}</Card.Header>
+        <Card.Header>Dog lovers</Card.Header>
+        <Card.Meta>GE #{ge.geId}</Card.Meta>
         <Card.Description>
-          test
+          <List>
+            <List.Item>
+              <List.Icon name="users" />
+              <List.Content>{members.length || "0"} members</List.Content>
+            </List.Item>
+            <List.Item>
+              <List.Icon name="numbered list" />
+              <List.Content>{tcxIds.length || "0"} TCXs</List.Content>
+            </List.Item>
+            <List.Item>
+              <List.Icon name="money bill alternate outline" />
+              <List.Content>{totalStaked || "0"} ATdot staked</List.Content>
+            </List.Item>
+            <List.Item>
+              <List.Icon name="money" />
+              <List.Content>{totalInvested || "0"} ATdot invested</List.Content>
+            </List.Item>
+          </List>
         </Card.Description>
       </Card.Content>
     </Card>
